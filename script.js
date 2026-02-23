0c828219a4ede5788aab8ca001a1d964ebc5777f
Krisna Eka Maulana <122388171+Hikris17@users.noreply.github.com>
2026-02-23 23:22:52 +0700

    Implement typing effect for text display
    Added a typing effect for displaying text phrases.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-document.querySelectorAll("a[href^='#']").forEach(anchor=>{
-anchor.addEventListener("click",function(e){
-e.preventDefault();
-document.querySelector(this.getAttribute("href"))
-.scrollIntoView({behavior:"smooth"});
-});
-});
+const text = ["Data Analyst", "Machine Learning Enthusiast", "Data Science Enthusiast"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type(){
+if(count === text.length) count = 0;
+
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+
+document.querySelector(".typing").textContent = letter;
+
+if(letter.length === currentText.length){
+count++;
+index = 0;
+}
+
+setTimeout(type,120);
+}
+
+type();
